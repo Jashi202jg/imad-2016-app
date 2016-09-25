419355bb36058e06e642157ec0a4735f000f6c94
Jashi202jg <jashi202jg@gmail.com>
2016-09-25 23:36:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hi, This is my new server';
 
 //Moving the image
-var image = document.getElementById('img');
+var image = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginleft = '100px';
 };
