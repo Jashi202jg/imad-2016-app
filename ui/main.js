37b941d61ef52f17d1c7314149bce70de11637f2
Jashi202jg <jashi202jg@gmail.com>
2016-09-25 23:33:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
-
+//Changing the text
 var element = document.getElementById('main-text');
 element.innerHTML = 'Hi, This is my new server';
+
+//Moving the image
+var image = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginleft = '100px';
+};
