7d025a80815d0c276c48b9e9b7c9fa04ae09d14e
Jashi202jg <jashi202jg@gmail.com>
2016-12-04 18:02:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ var htmlTemplate = `<html>
         <hr>
         <h4>${heading}</h4>
         <hr>
-        <h5>${date.toDateString()}</h5>
+        <h5>${date}</h5>
         <div>
         ${content}
         </div>
@@ -83,7 +83,7 @@ app.get('/test-db', function (req, res) {
     if(err){
         res.status(500).send(err.toString());
     } else{
-        res.send(JSON.stringify(result.rows));   
+        res.send(JSON.stringify(result));   
     }
  });
 });
@@ -104,25 +104,14 @@ app.get('/submit-name', function(req,res){  //URL:/submit-name?name=xxxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/articles/:articleName', function (req,res){
+app.get('/:articleName', function (req,res){
     
 //articleName == article-one
 //articles[articleName] == {} content object for article one
-//SELECT * FROM article WHERE title = 'article-one'
-  
-  pool.query("SELECT * FROM article WHERE title= '" + req.params.articleName +"'" , function(err,result){
-  if(err){
-        res.status(500).send(err.toString());  
-  }else{
-        if(result.rows.length=== 0){
-            res.status(404).send('Article not found'); 
-        } else {
-            var articleData = result.rows[0];
-            res.send(createTemplate(articleData));
-        }
-  }
- });
+ var articleName = req.params.articleName; 
+ res.send(createTemplate(articles[articleName]));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
