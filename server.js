c6bda48d3892adb7560266fd77f03e6280d1cc59
Jashi202jg <jashi202jg@gmail.com>
2016-12-16 23:12:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,6 @@ var content = data.content;
 var htmlTemplate = `<html>
   <head>
     <title>${title}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link href="/ui/style.css" rel="stylesheet" />
   </head>
     <body>
@@ -81,7 +80,7 @@ var htmlTemplate = `<html>
         <div class="container">
         <h4>${heading}</h4>
         <hr>
-        <h5>${date}</h5>
+        <h5>${date}</h5><hr>
         <div>
         ${content}
         </div>
