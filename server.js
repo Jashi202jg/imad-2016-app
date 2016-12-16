d7cd55566cb24cafa68afb797ad5027a731c82dd
Jashi202jg <jashi202jg@gmail.com>
2016-12-16 11:13:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-app.get('/ui/:madi.png', function (req, res) {
+app.get('/ui/madi.png', function (req, res) {
       
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
