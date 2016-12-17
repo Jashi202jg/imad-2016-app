1b1b24e8397aa3aa56acac65d5c02fca5bc7a3df
Jashi202jg <jashi202jg@gmail.com>
2016-12-17 22:29:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/Articles.html', function (req, res) {
+app.get('/Articles.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Articles.html'));
 });
 
